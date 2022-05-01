d4ff3ede89845ead0fd65dea592cd3ffacabeaed
Lzhilong <a932725344@163.com>
2022-05-01 12:00:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, Suspense } from "react";
+// import { renderRoutes } from "react-router-config";
+// import { Provider } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// import AppHeader from "@/components/AppHeader"
+export default memo(function App() {
+    return (
+        <div>
+           123
+        </div>
+    )
+})
---
+++ src/common/constants.js
@@ -0,0 +1,3 @@
+// 可以在此处定义常量
+export const HOT_RECOMMEND_LIMIT = 8;
+export const NEW_ALBUM_LIMIT = 10;
